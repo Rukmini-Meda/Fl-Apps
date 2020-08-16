9508f7eae903fe989a98296e3fe767be17781e5a
Rukmini-Meda <rukmini.m18@iiits.in>
2020-08-16 12:22:37 +0530

    Math Genius added

---
+++ MathGenius/android/app/src/main/kotlin/com/example/MathGenius/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.MathGenius
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Fl-Apps
+
+## MathGenius
+
+Don't bother. It's a simple calculator app ;)
